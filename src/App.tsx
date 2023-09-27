5bf0f2239572a7809718362fa662d24bd8bf4cab
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2023-09-27 13:06:17 +0200

    Header designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Footer from './common/components/Footer/Footer'
+import Header from './common/components/Header/Header'
+import Main from './common/components/Main/Main'
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
